3d6d6bb93fee235a16af446ec20c86e690b6cdf3
vVahe <vahe@hotmail.nl>
2019-08-11 09:10:14 +0200

    schema comments and underscored table names

--- models/users.js
+++ models/users.js
@@ -1,33 +1,42 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../util/db');
-const User = sequelize.define('users', {
-    user_id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+
+/**
+ * Define users table
+ * created_at & updated_at columns automatically added by ORM
+ */
+const User = sequelize.define(
+    'users',
+    {
+        user_id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        first_name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
     },
-    first_name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    last_name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    username: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-});
+    { underscored: true }
+);
 
 module.exports = User;
--- models/words.js
+++ models/words.js
@@ -1,26 +1,39 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../util/db');
-const Word = sequelize.define('words', {
-    word_id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+
+/**
+ * Define users table
+ * @word : word to be learned
+ * @word_meaning : translation of the @word to be learned
+ * @language : language of @word
+ * @category : category of @word
+ * @created_at & @updated_at columns automatically added by ORM
+ */
+const Word = sequelize.define(
+    'words',
+    {
+        word_id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        word: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+        word_meaning: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        description: Sequelize.STRING,
+        language: Sequelize.STRING,
+        category: {
+            type: Sequelize.STRING,
+            defaultValue: 'all'
+        }
     },
-    word: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-    },
-    word_meaning: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: Sequelize.STRING,
-    language: Sequelize.STRING,
-    category: {
-        type: Sequelize.STRING,
-        defaultValue: 'all'
-    }
-});
+    { underscored: true }
+);
 
 module.exports = Word;
