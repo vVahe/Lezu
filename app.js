d88b872fcef13e99c0c99e05e6344d3ca1cf01f6
vVahe <vahe@hotmail.nl>
2019-08-12 16:36:31 +0200

    add add_word route

--- app.js
+++ app.js
@@ -4,22 +4,39 @@ const passport = require('passport');
 
 const sequelize = require('./util/db');
 
+// import models
+const User = require('./models/users');
+const Word = require('./models/words');
+
+//set model associations
+User.belongsToMany(Word, {
+    through: 'UserWords',
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+Word.belongsToMany(User, {
+    through: 'UserWords',
+    foreignKey: 'word_id',
+    onDelete: 'cascade'
+});
+
 const app = express();
 const port = 3000;
 
 /** routers */
 const authRouter = require('./routes/auth');
+const wordsRouter = require('./routes/words');
 
 /** Body parser middleware */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-/** Passport middleware */
+/** Passport middleware & config */
 app.use(passport.initialize());
-/** Passport config */
 require('./util/passport')(passport);
 
 app.use('/auth', authRouter);
+app.use('/words', wordsRouter);
 
 app.listen(port, () => {
     console.log(`express listening on port ${port}`);
--- models/user_words.js
+++ models/user_words.js
@@ -1,35 +1,14 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/db');
+// const Sequelize = require('sequelize');
+// const sequelize = require('../util/db');
 
-const User = require('./users');
-const Word = require('./words');
+// const User = require('./users');
+// const Word = require('./words');
 
-/**
- * Define user_words table
- * @user_id & @word_id define which users has which words
- * @created_at & @updated_at columns automatically added by ORM
- */
-const UserWords = sequelize.define(
-    'user_words',
-    {
-        user_id: {
-            type: Sequelize.INTEGER,
-            references: {
-                model: User,
-                key: 'user_id'
-            },
-            primaryKey: true
-        },
-        word_id: {
-            type: Sequelize.INTEGER,
-            references: {
-                model: Word,
-                key: 'word_id'
-            },
-            primaryKey: true
-        }
-    },
-    { underscored: true }
-);
+// /**
+//  * Define user_words table
+//  * @user_id & @word_id define which users has which words
+//  * @created_at & @updated_at columns automatically added by ORM
+//  */
+// const UserWords = sequelize.define('user_words', {}, { underscored: true });
 
-module.exports = UserWords;
+// module.exports = UserWords;
