8872286851e421902fcc0c385d49c09825c92ed3
vVahe <vahe@hotmail.nl>
2019-08-19 17:48:41 +0200

    add footer and landing skeleton

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <Landing />
+            <Footer />
         </div>
     );
 }
--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Landing = () => {
-    return <div />;
+    return (
+        <div className="container landing p-5">
+            <h1>Welcome to Word Trainer</h1>
+        </div>
+    );
 };
 
 export default Landing;
