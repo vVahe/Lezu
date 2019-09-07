4dcb14be92fe827c4b3a56e0d1b8fed3c187abcc
vVahe <vahe@hotmail.nl>
2019-09-07 19:36:15 +0200

    add get all unreviewed words endpoint

--- routes/word-retrieve.js
+++ routes/word-retrieve.js
@@ -34,6 +34,38 @@ router.get(
     }
 );
 
+/**
+ * @route   GET /words-retrieve/unreviewed_words
+ * @desc    GET all words which have never been reviewed before
+ * @access  Private
+ */
+router.get(
+    '/unreviewed_words',
+    passport.authenticate('jwt', { session: false }),
+    (req, res, next) => {
+        const errors = {};
+
+        // find all words
+        Word.findAll({
+            where: { user_id: req.user.user_id, times_reviewed: 0 }
+        })
+            .then(words => {
+                // if no words were found return error
+                if (Object.keys(words).length === 0) {
+                    errors.words =
+                        'No words found that have not been reviewed yet';
+                    return res.status(400).json(errors);
+                }
+
+                // else return found words
+                return res.json(words);
+            })
+            .catch(err => {
+                return res.status(400).json(err);
+            });
+    }
+);
+
 // return recently created words (1 day, 1 week, 1 month ago)
 // return difficult words
 // return words by categories
