e80a11f6b5cd8adccd5ba5cfa826a765996f5c80
vVahe <vahe@hotmail.nl>
2019-09-11 15:05:52 +0200

    fix testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-    // const div = document.createElement('div');
-    // ReactDOM.render(<App />, div);
-    // ReactDOM.unmountComponentAtNode(div);
     //TODO: setup faker module to return dummy data to pass the test
 });
