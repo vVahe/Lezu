74d65d5232d56781c37626c7c3450a5e03542f10
vVahe <vahe@hotmail.nl>
2019-09-16 19:18:40 +0200

    add quick links to dashboard

---
+++ client/src/components/dashboard/QuickLinks.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function QuickLinks(props) {
+    return (
+        <React.Fragment>
+            <div className="row ml-1">
+                <h4 className="float-left text-secondary">Quick links</h4>
+            </div>
+            <div className="container mb-5">
+                <div className="row justify-content-center">
+                    <Link
+                        className="col-md-12 col-lg-6 card quick-links"
+                        to="/word-list"
+                    >
+                        <div class="card-body mx-auto">
+                            <i class="fa fa-list-alt fa-5x"></i>
+                            <h4>Word list</h4>
+                        </div>
+                    </Link>
+
+                    <Link
+                        className="col-md-12 col-lg-6 card quick-links"
+                        to="/word-list"
+                    >
+                        <div class="card-body">
+                            <i class="fa fa-check fa-5x"></i>
+                            <h4>Train words</h4>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
