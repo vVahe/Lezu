d9bb426c12ee6aa8744470c3fd59865a52b5bfbc
vVahe <vahe@hotmail.nl>
2019-09-17 16:23:30 +0200

    fix state queries

--- api/routes/stats.js
+++ api/routes/stats.js
@@ -15,45 +15,50 @@ router.get(
         const result = {};
         try {
             const q1 = await sequelize.query(
-                'SELECT COUNT(*) as nr_words FROM words',
+                'SELECT COUNT(*) as nr_words FROM words WHERE user_id = :user_id',
                 {
-                    type: sequelize.QueryTypes.SELECT
+                    type: sequelize.QueryTypes.SELECT,
+                    replacements: { user_id: Number(req.user.user_id) }
                 }
             );
             result[Object.keys(q1[0])[0]] = Number(
                 q1[0][Object.keys(q1[0])[0]]
             );
             const q2 = await sequelize.query(
-                'SELECT SUM(times_reviewed) as nr_words_reviewed FROM words',
+                'SELECT SUM(times_reviewed) as nr_words_reviewed FROM words WHERE user_id = :user_id',
                 {
-                    type: sequelize.QueryTypes.SELECT
+                    type: sequelize.QueryTypes.SELECT,
+                    replacements: { user_id: Number(req.user.user_id) }
                 }
             );
             result[Object.keys(q2[0])[0]] = Number(
                 q2[0][Object.keys(q2[0])[0]]
             );
             const q3 = await sequelize.query(
-                'SELECT SUM(times_correct) as nr_words_correct FROM words',
+                'SELECT SUM(times_correct) as nr_words_correct FROM words WHERE user_id = :user_id',
                 {
-                    type: sequelize.QueryTypes.SELECT
+                    type: sequelize.QueryTypes.SELECT,
+                    replacements: { user_id: Number(req.user.user_id) }
                 }
             );
             result[Object.keys(q3[0])[0]] = Number(
                 q3[0][Object.keys(q3[0])[0]]
             );
             const q4 = (result.nr_times_incorrect = await sequelize.query(
-                'SELECT SUM(times_incorrect) as nr_words_incorrect FROM words',
+                'SELECT SUM(times_incorrect) as nr_words_incorrect FROM words WHERE user_id = :user_id',
                 {
-                    type: sequelize.QueryTypes.SELECT
+                    type: sequelize.QueryTypes.SELECT,
+                    replacements: { user_id: Number(req.user.user_id) }
                 }
             ));
             result[Object.keys(q4[0])[0]] = Number(
                 q4[0][Object.keys(q4[0])[0]]
             );
             const q5 = await sequelize.query(
-                'SELECT DISTINCT COUNT(DISTINCT language_id) as nr_languages FROM words',
+                'SELECT DISTINCT COUNT(DISTINCT language_id) as nr_languages FROM words WHERE user_id = :user_id',
                 {
-                    type: sequelize.QueryTypes.SELECT
+                    type: sequelize.QueryTypes.SELECT,
+                    replacements: { user_id: Number(req.user.user_id) }
                 }
             );
             result[Object.keys(q5[0])[0]] = Number(
@@ -70,15 +75,17 @@ router.get(
                 q6[0][Object.keys(q6[0])[0]]
             );
             result.recent_words = await sequelize.query(
-                'SELECT * FROM words ORDER BY times_incorrect DESC LIMIT 10',
+                'SELECT * FROM words WHERE user_id = :user_id ORDER BY times_incorrect DESC LIMIT 10',
                 {
-                    type: sequelize.QueryTypes.SELECT
+                    type: sequelize.QueryTypes.SELECT,
+                    replacements: { user_id: Number(req.user.user_id) }
                 }
             );
             result.difficult_words = await sequelize.query(
-                'SELECT * FROM words ORDER BY created_at DESC LIMIT 10',
+                'SELECT * FROM words WHERE user_id = :user_id ORDER BY created_at DESC LIMIT 10',
                 {
-                    type: sequelize.QueryTypes.SELECT
+                    type: sequelize.QueryTypes.SELECT,
+                    replacements: { user_id: Number(req.user.user_id) }
                 }
             );
             return res.json(result);
