6ae8f26f50860a321549cbcdb5c36a9c1736ad5a
vVahe <vahe@hotmail.nl>
2019-09-18 17:50:39 +0200

    desplay message when no words are found

--- client/src/components/Wordlist/Wordlist.js
+++ client/src/components/Wordlist/Wordlist.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getWordlist, deleteWord } from '../../store/actions/wordlistActions';
 import { Link } from 'react-router-dom';
-
 import Word from './Word';
+const NoWords = require('../../images/no-words.svg');
 
 class Wordlist extends Component {
     state = {
@@ -30,57 +30,76 @@ class Wordlist extends Component {
 
         return (
             <div className="mt-5">
-                <h1 className="display-2">Word List</h1>
-                <div className="container mt-5">
-                    <Link
-                        to="/add-word"
-                        className="btn btn-lg btn-outline-success my-2 float-left"
-                    >
-                        <i className="fa fa-plus mr-2"></i> Add Word
-                    </Link>
-                    <table className="table table-striped shadow-lg bg-white">
-                        <thead className="thead-dark">
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">
-                                    <i className="fa fa-language"></i>
-                                </th>
-                                <th scope="col">Word</th>
-                                <th scope="col">Translastion</th>
-                                <th scope="col">
-                                    <i className="fa fa-i-cursor"></i>
-                                </th>
-                                <th scope="col">
-                                    <i className="fa fa-check"></i>
-                                </th>
-                                <th scope="col">
-                                    <i className="fa fa-times"></i>
-                                </th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {words.map(word => {
-                                rowNumber++;
-                                return (
-                                    <Word
-                                        key={word.word_id}
-                                        word_id={word.word_id}
-                                        rowNumber={rowNumber}
-                                        word={word.word}
-                                        word_meaning={word.word_meaning}
-                                        times_reviewed={word.times_reviewed}
-                                        times_correct={word.times_correct}
-                                        times_incorrect={word.times_incorrect}
-                                        language={word.language_id}
-                                        delete={this.deleteWordHandler}
-                                    />
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+                {words.length > 0 && (
+                    <div className="container mt-5">
+                        <Link
+                            to="/add-word"
+                            className="btn btn-lg btn-outline-success my-2 float-left"
+                        >
+                            <i className="fa fa-plus mr-2"></i> Add Word
+                        </Link>
+                        <table className="table table-striped shadow-lg bg-white">
+                            <thead className="thead-dark">
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">
+                                        <i className="fa fa-language"></i>
+                                    </th>
+                                    <th scope="col">Word</th>
+                                    <th scope="col">Translastion</th>
+                                    <th scope="col">
+                                        <i className="fa fa-i-cursor"></i>
+                                    </th>
+                                    <th scope="col">
+                                        <i className="fa fa-check"></i>
+                                    </th>
+                                    <th scope="col">
+                                        <i className="fa fa-times"></i>
+                                    </th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {words.map(word => {
+                                    rowNumber++;
+                                    return (
+                                        <Word
+                                            key={word.word_id}
+                                            word_id={word.word_id}
+                                            rowNumber={rowNumber}
+                                            word={word.word}
+                                            word_meaning={word.word_meaning}
+                                            times_reviewed={word.times_reviewed}
+                                            times_correct={word.times_correct}
+                                            times_incorrect={
+                                                word.times_incorrect
+                                            }
+                                            language={word.language_id}
+                                            delete={this.deleteWordHandler}
+                                        />
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+                {words.length < 1 && (
+                    <div>
+                        <h4>No words found!</h4>
+                        <img
+                            alt="No words founds mb-5"
+                            height="150vh"
+                            src={NoWords}
+                        />
+                        <Link
+                            to="/add-word"
+                            className="btn d-block btn-lg btn-outline-success mt-5 w-25 mx-auto"
+                        >
+                            <i className="fa fa-plus mr-2"></i> Add Word
+                        </Link>
+                    </div>
+                )}
             </div>
         );
     }
